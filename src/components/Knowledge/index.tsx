a566008074b95edadde49bf5f73183c334fa7aa7
LeonardoBrum0907 <leobrum0907@gmail.com>
2022-07-30 17:54:26 -0300

    create knowledge component

---
+++ src/components/Knowledge/index.tsx
@@ -0,0 +1,20 @@
+import { AiFillHtml5 } from 'react-icons/ai';
+import { FaCss3Alt, FaReact } from 'react-icons/fa';
+import { IoLogoJavascript } from 'react-icons/io';
+import { SectionTitle } from '../SectionTitle';
+import { KnowledgeItem } from './KnowledgeItem';
+import { Container } from './styles';
+
+export function Knowledge() {
+  return (
+    <Container>
+      <SectionTitle title="Knowledge" />
+      <section>
+        <KnowledgeItem title="HTML" icon={<AiFillHtml5 />} />
+        <KnowledgeItem title="CSS" icon={<FaCss3Alt />} />
+        <KnowledgeItem title="javascript" icon={<IoLogoJavascript />} />
+        <KnowledgeItem title="Reactjs" icon={<FaReact />} />
+      </section>
+    </Container>
+  );
+}
