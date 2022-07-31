193680a930478aaa9324b55b260cce5cec28f332
LeonardoBrum0907 <leobrum0907@gmail.com>
2022-07-31 15:30:52 -0300

    add new components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { HomeHero } from '../components/HomeHero';
 // import { Experience } from '../components/Experience';
 import { Projects } from '../components/Projects';
 import { Knowledge } from '../components/Knowledge';
+import { FormContact } from '../components/FormContact';
+import { Footer } from '../components/Footer';
 
 export default function Home() {
   return (
@@ -16,6 +18,8 @@ export default function Home() {
         {/* <Experience /> */}
         <Projects />
         <Knowledge />
+        <FormContact />
+        <Footer />
       </main>
     </HomeContainer>
   );
