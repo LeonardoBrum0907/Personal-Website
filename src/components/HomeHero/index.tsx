22b1346bfd62737273cfeccabab0e85b78b50589
Leonardo Brum <leobrum0907@gmail.com>
2023-01-28 20:10:45 -0300

    HomeHero component translate

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,36 +1,43 @@
+import { useTranslation } from 'react-i18next';
 import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
 import pictures from '../../assets/minhaFoto.png';
 
 export function HomeHero() {
+  const { t } = useTranslation();
+
   return (
     <Container data-aos="fade-up">
       <img src={pictures} alt="Minha foto" />
       <div>
         <TextContainer>
-          <h1>Hi!</h1>
+          <h1>{t('welcome')}</h1>
         </TextContainer>
         <InfosContainer>
           <span className="spanAnimation1">
-            I&apos;m Léo, Dev <span className="text-white">Front-end</span>.
+            {t('welcome-subtitle')}
+            <span className="text-white">Front-end</span>.
           </span>
           <CodeItem>
-            <span className="comment">&frasl;&frasl; my presentation</span>
+            <span className="comment">{t('my-presentation')}</span>
             <span className="purple">Infos</span> {'\u007B'}
             <div>
-              name: <span className="blue">&quot;Leonardo Brum&quot;</span>,
+              {t('name')}
+              <span className="blue">&quot;Leonardo Brum&quot;</span>,
             </div>
             <div>
-              age: <span className="blue">&quot;20 years&quot;</span>,
+              {t('age')}
+              <span className="blue">&quot;20{t('years')}</span>,
             </div>
             <div>
-              city: <span className="blue">&quot;Hortolândia-SP&quot;</span>,
+              {t('city')}
+              <span className="blue">&quot;Hortolândia-SP&quot;</span>,
             </div>
             {'\u007D'}
           </CodeItem>
           <CodeItem>
-            <span className="purple">Contact</span> {'\u007B'}
+            <span className="purple">{t('contact')}</span> {'\u007B'}
             <div>
-              phoneNumber:{' '}
+              {t('phone-number')}
               <span className="blue">&quot;(19) 99965-5577&quot;</span>,
             </div>
             <div>
